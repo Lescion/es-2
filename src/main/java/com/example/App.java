a428435ef7e6e799c47e456215f238e87bfcce91
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:47:18 +0200

    quasi finito

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public final class App {
     private App() {
     }
@@ -10,8 +14,24 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException 
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+        Socket miosocket = mioServerSocket.accept();
+        System.out.println("qualcuno si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(miosocket.getInputStream()));
+        PrintWriter out = new PrintWriter(miosocket.getOutputStream(), true);
+
+        do{
+        String stringricevuta = in.readLine();
+        String stringMaiuscola = stringricevuta.toUpperCase();
+        out.println(stringMaiuscola);
+        }while(in.readLine().equals("!"));
+
+
+        mioServerSocket.close();
     }
 }
